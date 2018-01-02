acba5fa4add3a52db0d08b93b4d49a24fc04dcf5
c0ny1 <1627639645@qq.com>
2018-01-03 00:28:31 +0800

    写好单个验证窗口UI

--- src/com/batSSH/gui/CheckFrame.java
+++ src/com/batSSH/gui/CheckFrame.java
@@ -6,109 +6,83 @@ import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
 
-public class CheckFrame extends JFrame {
+public class CheckFrame extends JDialog {
 	private String name = "check";
 	
 	CheckFrame(){
 		this.setTitle(name);
-		this.setSize(800, 250);
+		this.setSize(750, 350);
+		this.setResizable(false);
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		int x = (int)(toolkit.getScreenSize().getWidth()-this.getWidth())/2;
 		int y = (int)(toolkit.getScreenSize().getHeight()-this.getHeight())/2;
 		this.setLocation(x,y);
-		
-		this.setLayout(new BorderLayout());
-		
-		
-		
-		
+		this.setLayout(new GridLayout(1,1));	
 		
 		//右边
-		JPanel LPanel = new JPanel();
-		Box box_host,box_port,box_login_user,box_login_pwd,box_root_user,box_root_pwd,box_btn,box_base;
-		box_base = Box.createHorizontalBox();//水平盒式布局
+		JPanel Panel = new JPanel();
+		Panel.setLayout(new GridBagLayout());
+		//Panel.setBackground(Color.BLACK);
 		
-		box_host = Box.createHorizontalBox();
-		box_port = Box.createHorizontalBox();
-		box_login_user = Box.createHorizontalBox();
-		box_login_pwd = Box.createHorizontalBox();
-		box_root_user = Box.createHorizontalBox();
-		box_root_pwd = Box.createHorizontalBox();
-		box_btn = Box.createHorizontalBox();
-		box_base = Box.createVerticalBox();
+		Panel.add(new JLabel("Host:"),new GBC(0,0,1,1).setFill(GBC.BOTH).setInsets(10,10,2,0));
+		Panel.add(new JTextField(2), new GBC(1,0,2,1).setFill(GBC.BOTH).setFill(GBC.BOTH).setInsets(10,0,2,10));
+		Panel.add(new JLabel("Port:"),new GBC(0,1,1,1).setFill(GBC.BOTH).setInsets(10,10,2,0));
+		Panel.add(new JTextField(2),new GBC(1,1,2,1).setFill(GBC.BOTH).setInsets(10,0,2,10));
+		Panel.add(new JLabel("Login Username:"),new GBC(0,2,1,1).setFill(GBC.BOTH).setInsets(10,10,2,0));
+		Panel.add(new JTextField(2),new GBC(1,2,2,1).setFill(GBC.BOTH).setInsets(10,0,2,10));
+		Panel.add(new JLabel("Login Password:"),new GBC(0,3,1,1).setFill(GBC.BOTH).setInsets(10,10,2,0));
+		Panel.add(new JTextField(2),new GBC(1,3,2,1).setFill(GBC.BOTH).setInsets(10,0,2,10));
+		Panel.add(new JLabel("Root Username:"),new GBC(0,4,1,1).setFill(GBC.BOTH).setInsets(10,10,2,0));
+		Panel.add(new JTextField(2),new GBC(1,4,2,1).setFill(GBC.BOTH).setInsets(10,0,2,10));
+		Panel.add(new JLabel("Root Password:"),new GBC(0,5,1,1).setFill(GBC.BOTH).setInsets(10,10,2,0));
+		Panel.add(new JTextField(2),new GBC(1,5,2,1).setFill(GBC.BOTH).setInsets(10,0,2,10));	
+		Panel.add(new JButton("Check"),new GBC(1,6,1,1).setInsets(10,10,10,10));
+		Panel.add(new JButton("Open shell"),new GBC(2,6,1,1).setFill(GBC.BOTH).setFill(GBC.BOTH).setInsets(10,10,10,10));
 		
-		box_host.add(new JLabel("Host:"));
-		box_host.add(new JTextField(10));
-		box_port.add(new JLabel("Port:"));
-		box_port.add(new JTextField(10));
-		box_login_user.add(new JLabel("Login Username:"));
-		box_login_user.add(new JTextField(10));
-		box_login_pwd.add(new JLabel("Login Password:"));
-		box_login_pwd.add(new JTextField(10));
-		box_root_user.add(new JLabel("Root Username:"));
-		box_root_user.add(new JTextField(10));
-		box_root_pwd.add(new JLabel("Root Password:"));
-		box_root_pwd.add(new JTextField(10));	
-		box_btn.add(new JButton("Check"));
-		box_btn.add(new JButton("Open shell"));
+
+	
 		
-		box_base.add(box_host);
-		box_base.add(box_port);
-		box_base.add(box_login_user);
-		box_base.add(box_login_pwd);
-		box_base.add(box_root_user);
-		box_base.add(box_root_pwd);
-		box_base.add(box_btn);
-		
-		LPanel.add(box_base);
-		
-		
-		//右边
-		JPanel RPanel = new JPanel();
-		Box box_1,box_2,box_3;
-		
-		box_1 = Box.createHorizontalBox();
-		box_2 = Box.createHorizontalBox();
-		box_3 = Box.createVerticalBox();
-		
-		JTextArea taShell = new JTextArea(10,40);
+		JTextArea taShell = new JTextArea(5,45);
 		taShell.setBackground(new Color(0, 0, 0));
 		taShell.setForeground(Color.GREEN);
 		taShell.setSelectedTextColor(new Color(255, 255, 255));
 		JScrollPane scrShell = new JScrollPane(taShell);
-		box_1.add(scrShell);
+		Panel.add(scrShell,new GBC(3,0,12,6).setFill(GBC.BOTH).setInsets(10,0,0,10));
 		
-		JTextField tfCmd = new JTextField(5);
+		JTextField tfCmd = new JTextField(50);
 		JButton btnExec = new JButton("exec");
-		box_2.add(tfCmd);
-		box_2.add(Box.createHorizontalStrut(5));
-		box_2.add(btnExec);
-		
-		box_3.add(box_1);
-		box_3.add(Box.createVerticalStrut(10));
-		box_3.add(box_2);
-		
-		RPanel.add(box_3);
-		
-		this.add(LPanel,BorderLayout.WEST);
-		this.add(RPanel,BorderLayout.EAST);
-
-		
+		Panel.add(tfCmd, new GBC(3,6,11,1).setFill(GBC.BOTH).setWeight(100, 0).setInsets(10,0,10,2));
+		Panel.add(btnExec, new GBC(14,6,1,1).setInsets(10,0,10,10));
+		this.add(Panel);
 	}
 	
 	public static void main(String[] args){
+		//设置皮肤
+		try
+	    {
+			UIManager.put("RootPane.setupButtonVisible",false);
+	        org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+	    }
+	    catch(Exception e)
+	    {
+	    	System.out.println(e.getMessage());
+	    }
 		CheckFrame frm = new CheckFrame();
 		frm.setVisible(true);
 	}
